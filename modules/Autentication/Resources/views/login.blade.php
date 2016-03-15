7d08976e8f808b7c21bffbaebdc50922da91c6fb
Jose A. Ricardo Lara <ricardoingenierias@gmail.com>
2016-03-15 06:24:03 -0500

    Actualizando cambios

--- modules/Autentication/Resources/views/login.blade.php
+++ modules/Autentication/Resources/views/login.blade.php
@@ -12,7 +12,7 @@
           
           <div class="row">
             <div class="input-field col s12">
-              <input id="email" name="email" type="email" value="jarl1109@gmail.com" class="validate" value="{{ old('email') }}" autocomplete="off" >
+              <input id="email" name="email" type="email" value="jarl_47@hotmail.com" class="validate" value="{{ old('email') }}" autocomplete="off" >
               <label for="email">Email</label>
               @if ($errors->has('email'))
                   <span class="help-block">
@@ -24,7 +24,7 @@
 
           <div class="row">
             <div class="input-field col s12">
-              <input id="password" name="password" type="password" value="monteria1109" class="validate">
+              <input id="password" name="password" type="password" value="1234567" class="validate">
               <label for="password">Password</label>
               @if ($errors->has('password'))
                 <span class="help-block">
--- modules/Generator/Resources/views/index.blade.php
+++ modules/Generator/Resources/views/index.blade.php
@@ -6,8 +6,33 @@
 
 @section('content')
 
+<div class="col s12 m12 l12">
+<br>
+  <div class="chip teal lighten-4 z-depth-1 hoverable">
+        <a href="{!! url('/projects') !!}">
+            Projects 
+            <i class="material-icons">keyboard_arrow_right</i>
+        </a>
+        
+  </div>
+  <div class="chip teal lighten-4 z-depth-1">
+       <a href="#">
+           List Tables 
+       </a>
+       
+  </div>
+  
+</div> 
+<br>
+<div class="row">
+    
+    <div class="col s12 card-panel z-depth-1 grey lighten-3" >
+    	dfsdfsdfsd
+    </div> 
+   
+  </div>
+
  
- {!! $projects !!}
 <div class="list_generator">
     <div class="indicador_carga"></div>      
    
